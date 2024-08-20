14ac5044d59e5714429a806275582a2d0631da3d
prashant <manprashant99@gmail.com>
2024-08-20 21:08:16 +0530

    first commit

--- lib/utils/helpers/pricingCalculator.dart
+++ lib/utils/helpers/pricingCalculator.dart
@@ -1,3 +1,32 @@
 class SPricingCalculator{
+  static double calculateTotalPrice(double productPrice,String location){
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+
+    double shippingCart = getShippingCost(location);
+    double totalPrice = productPrice + taxAmount + shippingCart;
+    return totalPrice;
+  }
+
+  static String calculateShippingCost(double productPrice,String location){
+    double shippingCost = getShippingCost(location);
+    return shippingCost.toStringAsFixed(2);
+  }
+
+  static String calculateTax(double productPrice,String location){
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+
+    return taxAmount.toStringAsFixed(2);
+  }
+
+  static double getTaxRateForLocation(String location){
+    return 0.10;
+  }
+  static double getShippingCost(String location){
+    return 5.00;
+  }
+
+
 
 }
---
+++ lib/utils/validators/validation.dart
@@ -0,0 +1,48 @@
+class SValidator{
+
+  static String? validateEmail(String? value){
+    if(value == null || value.isEmpty){
+      return 'Email is Required';
+    }
+
+    final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if(!emailRegExp.hasMatch(value)){
+      return 'Invalid email address';
+    }
+
+    return null;
+
+  }
+
+
+  static String? validatePassword(String? value){
+    if(value == null || value.isEmpty){
+      return 'Password is required.';
+    }
+
+    if(value.length<6){
+      return 'Password must be at least 6 characters long.';
+
+    }
+    if(!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))){
+      return 'Password must contain at least one special characters';
+    }
+
+    return null;
+  }
+
+  static String? validatePhoneNumber(String? value){
+    if(value == null || value.isEmpty){
+      return 'Phone number is required';
+    }
+    final phoneReqExp = RegExp(r'^\d{10}$');
+    if(!phoneReqExp.hasMatch(value)){
+      return 'Invalid phone number format (10 digits required).';
+    }
+
+    return null;
+  }
+
+
+
+}
