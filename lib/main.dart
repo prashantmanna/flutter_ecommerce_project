bdb02843e8824f903ea24176f891fa2c7591561a
prashant <manprashant99@gmail.com>
2024-08-22 10:49:17 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,51 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_project/app.dart';
-import 'package:flutter_ecommerce_project/utils/constants/colors.dart';
-import 'package:flutter_ecommerce_project/utils/theme/theme.dart';
 
 void main() {
-
-  runApp(const App());
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Splash(), // Your Splash screen
+    );
+  }
+}
 
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 4), () =>
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context)=> App()))
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/app.png"),
+            Text("Shopnow",style: TextStyle(fontWeight: FontWeight.bold,fontStyle: FontStyle.italic),)
+          ],
+        ),
+      ),
+    );
+  }
+}
