4e0e2f42653d966713af078e26f8fbdcc7a4e988
prashant <manprashant99@gmail.com>
2024-09-11 22:44:11 +0530

    first commit

--- lib/features/shop/screens/store/widget/category_tab.dart
+++ lib/features/shop/screens/store/widget/category_tab.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_project/common/layouts/SGridLayout.dart';
 import 'package:flutter_ecommerce_project/common/widgets/product_text/Sectionheading.dart';
+import 'package:flutter_ecommerce_project/common/widgets/products_cards/ProductVertical.dart';
 
 import '../../../../../common/widgets/products_cards/SBrandProducts.dart';
 import '../../../../../utils/constants/image_strings.dart';
@@ -10,21 +12,33 @@ class CategoryTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(padding: const EdgeInsets.all(SSizes.defaultSpace),
-      child: Column(
-        children: [
-          const SBrandProducts(
-            images: [
-              SImages.watch1,
-              SImages.watch2,
-              SImages.watch3
-            ],
-            
-          ),
-          SizedBox(height: SSizes.spaceBtwItems,),
-          Sectionheading(title: "You might like",onPressed:(){} ,),
-          const SizedBox(height: SSizes.spaceBtwSections,)
-        ],
-      ),);
+    return  ListView(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      children:[Padding(padding: const EdgeInsets.all(SSizes.defaultSpace),
+        child: Column(
+          children: [
+            const SBrandProducts(
+              images: [
+                SImages.watch1,
+                SImages.watch2,
+                SImages.watch3
+              ],
+
+            ),
+            const SBrandProducts(images: [
+              SImages.laptop1,
+              SImages.laptop2,
+              SImages.laptop3
+            ]),
+            const SizedBox(height: SSizes.spaceBtwItems,),
+            Sectionheading(title: "You might like",onPressed:(){} ,),
+            const SizedBox(height: SSizes.spaceBtwSections,),
+
+            SGridLayout(itemCount: 4, itemBuilder: (_,index)=>const ProductVertical()),
+            const SizedBox(height: SSizes.spaceBtwItems,)
+          ],
+        ),),]
+    );
   }
 }
