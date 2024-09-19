5645d1095a758733f39af19e9cbcf09f679c9ca8
prashant <manprashant99@gmail.com>
2024-09-19 18:24:52 +0530

    second commit

--- lib/features/authentication/controllers/signup/signup_controller.dart
+++ lib/features/authentication/controllers/signup/signup_controller.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_project/utils/constants/Loaders.dart';
 import 'package:flutter_ecommerce_project/utils/pop_ups/full_screen_loader.dart';
 import 'package:get/get.dart';
+
+import '../../../../utils/device/NetworkManager.dart';
 class SignupController extends GetxController{
   static SignupController get instance => Get.find();
 
@@ -15,10 +18,22 @@ class SignupController extends GetxController{
   Future<void> signup_key_controller() async{
     try{
       FullScreenLoader.openLoadingDialog("We are processing your information", "");
+      //check internet connection
+      final isConnected = await NetworkManager.instance.isConnected();
+
+      if(!isConnected){
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      if(!signUpFormKey.currentState!.validate()){
+        FullScreenLoader.stopLoading();
+        return;
+      }
     }catch(e){
-
+      Loaders.errorSnackBar(title: "Oh Snap! ",message: e.toString());
     }finally{
-
+      FullScreenLoader.stopLoading();
     }
   }
 }
---
+++ lib/utils/device/NetworkManager.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import '../constants/Loaders.dart';
+
+class NetworkManager extends GetxController {
+  static NetworkManager get instance => Get.find();
+
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+  final Rx<ConnectivityResult> _connectionStatus = ConnectivityResult.none.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _connectivitySubscription = _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    _connectionStatus.value = result;
+    if (_connectionStatus.value == ConnectivityResult.none) {
+      Loaders.warningSnackBar(title: "No Internet Connection");
+    }
+  }
+
+  Future<bool> isConnected() async {
+    try {
+      final result = await _connectivity.checkConnectivity();
+      return result != ConnectivityResult.none;
+    } on PlatformException catch (_) {
+      return false;
+    }
+  }
+
+  @override
+  void onClose() {
+    _connectivitySubscription.cancel();
+    super.onClose();
+  }
+}
