636245fd55936e432f272dfb200e507dab10b0b7
prashant <manprashant99@gmail.com>
2024-09-21 11:25:41 +0530

    second commit

--- lib/app.dart
+++ lib/app.dart
@@ -3,10 +3,8 @@ import 'package:flutter_ecommerce_project/utils/constants/colors.dart';
 import 'package:flutter_ecommerce_project/utils/helpers/helper_functions.dart';
 import 'package:flutter_ecommerce_project/utils/theme/theme.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-
 class App extends StatelessWidget {
   const App({super.key});
-
   @override
   Widget build(BuildContext context) {
     final dark = SHelperFunctions.isDarkMode(context);
