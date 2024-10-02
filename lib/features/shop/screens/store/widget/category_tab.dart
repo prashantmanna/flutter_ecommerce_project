cb7479219c3bd61cf9913438fd6078641e15477c
prashant <manprashant99@gmail.com>
2024-10-02 21:26:57 +0530

    third commit

--- lib/features/shop/screens/store/widget/category_tab.dart
+++ lib/features/shop/screens/store/widget/category_tab.dart
@@ -20,6 +20,9 @@ class CategoryTab extends StatelessWidget {
           children: [
             const SBrandProducts(
               images: [
+
+
+
                 SImages.watch1,
                 SImages.watch2,
                 SImages.watch3
